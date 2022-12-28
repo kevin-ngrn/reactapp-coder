0344f0143f03411fce62ad1ce1d87015bb356c6c
kevin <kevonegrin@gmail.com>
2022-12-28 19:21:22 -0300

    pre-entrega app react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {NavBar} from './components/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ItemListContainer } from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <NavBar/>
+      <div className='ItemListContainer'>
+      <ItemListContainer greeting={"Bienvenido/a"}/>
+      </div>
     </div>
   );
 }
