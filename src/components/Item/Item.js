39fc338eb21ca33d97bc86b2fc89262ec1b800d0
kevin <kevonegrin@gmail.com>
2023-01-11 22:58:56 -0300

    .

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,6 +2,16 @@
 
 
 
-export const Item = () => {
-    
+export const Item = ({id, name, description, image, price, category}) => {
+    return(
+        <div>
+            <img src={image} alt={name}></img>
+            <h4>{name}</h4>
+            <p>{price}</p>
+            <p>{description}</p>
+            <small>{category}</small>
+        </div>
+    )
 } 
+
+export default Item
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react'
 import './ItemListContainer.css'
+import stock from '../../Data/Mock_Data.json'
+import Item from '../Item/Item.js'
 
 
+export const ItemListContainer = () => {
 
-export const ItemListContainer = ({encabezado}) => {
-    return(
-        <h1 className='my-5 itemListContainer'>{encabezado}</h1>
+const pedirDatos = () => {
+        return new Promise ((resolve) =>{
+            setTimeout (() => {          
+                resolve (stock)   
+            }, 2000)
+        })
+    } 
+
+const  [stock, setStock] = useState ([])
+
+    useEffect (() => {
+        pedirDatos()
+            .then((res)=> {
+                console.log(res);
+            })
+            .catch((err) =>{
+                console.log(err);
+            })
+    }, [])
+
+    return (
+        <div className='container my-5 '>
+        </div>
     )
+
 }
 
+export default ItemListContainer
