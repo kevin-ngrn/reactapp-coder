d13bb5f1e8689c5e303ed827c5fd49a84e0f8401
kevin <kevonegrin@gmail.com>
2023-02-17 03:10:51 -0300

    PF MOCK-DATA

--- src/components/OpinionScreen/OpinionScreen.js
+++ src/components/OpinionScreen/OpinionScreen.js
@@ -5,15 +5,16 @@ const OpinionScreen = () => {
 
     return(
     <div>
-        <h2 className="h2Form">¡Nos gustaria conocer tu experiencia con Burger Point!</h2>
+        <h2 className="h2Form">¡Nos gustaria conocer tu experiencia en Burger Point!</h2>
         <hr></hr>
         <div className="divForm">
-        <form className="imgForm">
+        <form>
             <h4>Nombre:</h4>
             <input className="form-control" type={"text"}></input>
             <h4>Email:</h4>
             <input className="form-control" type={"email"}></input>
             <textarea className="form-control inputMensaje" type={"text"} placeholder="¡Dejanos tu comentario!"></textarea>
+            <button className="btn btn-primary btnEnviar">Enviar</button>
         </form>
         </div>
     </div>
